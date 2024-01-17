fdf63c5c3dbb62e2d85ec3371ccd24c75d9b7c15
sleepingburrito <45605520+sleepingburrito@users.noreply.github.com>
2024-01-16 21:52:36 -0600

    started on game scene

---
+++ gameScene.py
@@ -0,0 +1,30 @@
+import pygame
+import gamePlayer
+import gameWall
+
+_playersList: list[gamePlayer.player] = []
+_wallsList: list[gameWall.wall] = []
+
+#players
+def InitPlayers() -> None:
+    pass
+
+
+#walls
+def InitWalls() -> None:
+    pass
+
+def ResetWalla() -> None:
+    pass
+
+
+#everything
+
+def TickPlayer() -> None:
+    pass
+
+def TickPlayerWallInteraction() -> None:
+    pass
+
+def TickEverything() -> None:
+    pass
